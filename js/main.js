addf1a80bbd3ee83e3f2c1f8926c66cee07b08e0
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-04-17 00:12:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function getUserDataProfile(url) {
+  return parseJSON(apiGET(url))
+}
+
 function apiGET(url) {
   let request = new XMLHttpRequest()
   request.open("GET", url, false)
@@ -5,10 +9,31 @@ function apiGET(url) {
   return request.responseText
 }
 
+function parseJSON(res) {
+  let user = JSON.parse(res)
+  return user
+}
+
 function main() {
-  let data = apiGET("http://api.github.com/users/birobirobiro");
-  let user = JSON.parse(data);
-  console.log(user)
+  const user_obj = getUserDataProfile("http://api.github.com/users/birobirobiro")
+  const user_name = document.querySelector('.user-name')
+  
+  const profile_pic = document.querySelector('.profile-picture>img')
+  const followers = document.querySelector('.text-followers')
+  const following = document.querySelector('.text-following')
+  const repos = document.querySelector('.text-repos')
+  const company = document.querySelector('.text-company')
+  const location = document.querySelector('.text-location')
+
+
+  user_name.innerHTML = user_obj.login
+  profile_pic.src = user_obj.avatar_url
+  followers.innerHTML = user_obj.followers
+  following.innerHTML = user_obj.following
+  repos.innerHTML = user_obj.public_repos
+  company.innerHTML = user_obj.company
+  location.innerHTML = user_obj.location
+  
 }
 
 main()
